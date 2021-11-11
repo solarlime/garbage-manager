4c3e682f918f9ed2d5b8c1899b22214a5342f4a9
solarlime <solarlime@outlook.com>
2021-11-11 23:16:40 +0300

    Replaced flex with masonry

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,32 @@
+/* eslint-disable no-param-reassign */
+import Masonry from 'masonry-layout';
+
 export default class App {
   static init() {
-    function a(b: number): number {
-      return b ** 2;
-    }
+    const spoilers: NodeListOf<Element> = document.querySelectorAll('.item-delete-spoiler');
+    spoilers
+      .forEach((spoiler: Element): void => { (spoiler as HTMLInputElement).checked = false; });
 
-    const c: number = 5;
+    const grid = document.querySelector('.app-view-list');
+    const masonry = new Masonry(grid, {
+      gutter: 20,
+      itemSelector: '.app-view-list-item',
+      percentPosition: true,
+      columnWidth: '.app-view-list-item',
+    });
 
-    console.log(a(c));
+    grid.addEventListener('click', (event: Event) => {
+      const target = event.target as Element;
+      if (target.matches('.item-delete-spoiler')) {
+        Array.from(spoilers)
+          .filter((spoiler): Boolean => (spoiler !== target)
+              && ((spoiler as HTMLInputElement).checked === true))
+          .forEach((spoiler): void => { (spoiler as HTMLInputElement).checked = false; });
+        const timeout = setTimeout(() => {
+          masonry.layout();
+          clearTimeout(timeout);
+        }, 100);
+      }
+    });
   }
 }
