2e4f39220308488a6212fdd3f159864cc17bf228
solarlime <solarlime@outlook.com>
2021-11-13 02:15:29 +0300

    Bugfix: masonry could render items incorrectly

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -7,26 +7,28 @@ export default class App {
     spoilers
       .forEach((spoiler: Element): void => { (spoiler as HTMLInputElement).checked = false; });
 
-    const grid = document.querySelector('.app-view-list');
-    const masonry = new Masonry(grid, {
-      gutter: 20,
-      itemSelector: '.app-view-list-item',
-      percentPosition: true,
-      columnWidth: '.app-view-list-item',
-    });
+    window.addEventListener('load', () => {
+      const grid = document.querySelector('.app-view-list');
+      const masonry = new Masonry(grid, {
+        gutter: 20,
+        itemSelector: '.app-view-list-item',
+        percentPosition: true,
+        columnWidth: '.app-view-list-item',
+      });
 
-    grid.addEventListener('click', (event: Event) => {
-      const target = event.target as Element;
-      if (target.matches('.item-delete-spoiler')) {
-        Array.from(spoilers)
-          .filter((spoiler): Boolean => (spoiler !== target)
-              && ((spoiler as HTMLInputElement).checked === true))
-          .forEach((spoiler): void => { (spoiler as HTMLInputElement).checked = false; });
-        const timeout = setTimeout(() => {
-          masonry.layout();
-          clearTimeout(timeout);
-        }, 100);
-      }
-    });
+      grid.addEventListener('click', (event: Event) => {
+        const target = event.target as Element;
+        if (target.matches('.item-delete-spoiler')) {
+          Array.from(spoilers)
+            .filter((spoiler): Boolean => (spoiler !== target)
+                && ((spoiler as HTMLInputElement).checked === true))
+            .forEach((spoiler): void => { (spoiler as HTMLInputElement).checked = false; });
+          const timeout = setTimeout(() => {
+            masonry.layout();
+            clearTimeout(timeout);
+          }, 100);
+        }
+      });
+    }, { once: true });
   }
 }
